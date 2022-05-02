3c797ab7539b123bc58c38f7c63ea9527beb03f7
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-02 11:36:48 +0400

    const hourElement = document.querySelector(".hour"); const minuteElement = document.querySelector(".minute"); const secondElement = document.querySelector(".second"); const millisecondElement = document.querySelector(".millisecond");

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// timer fields
+
+const hourElement = document.querySelector(".hour");
+const minuteElement = document.querySelector(".minute");
+const secondElement = document.querySelector(".second");
+const millisecondElement = document.querySelector(".millisecond");
