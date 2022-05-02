cdaaddf988c4bb746f51003548e7e5571c515ae8
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-02 11:39:37 +0400

    //получим кнопки //Bottons

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ const hourElement = document.querySelector(".hour");
 const minuteElement = document.querySelector(".minute");
 const secondElement = document.querySelector(".second");
 const millisecondElement = document.querySelector(".millisecond");
+
+//получим кнопки
+//Bottons
+const startButton = document.querySelector(".start");
+const pauseButton = document.querySelector(".pause");
+const stopButton = document.querySelector(".stop");
+const newButton = document.querySelector(".new");
