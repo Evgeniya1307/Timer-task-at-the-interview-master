2b1094bb1d109f279648c004ce5f36ad84acbd63
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-05-02 12:41:02 +0400

    function disabledBtn() {   if (disabled) {     newButton.disabled = true;   }

--- js/main.js
+++ js/main.js
@@ -6,44 +6,45 @@ const secondElement = document.querySelector(".second");
 const millisecondElement = document.querySelector(".millisecond");
 
 //получим кнопки
-// Bottons--------------------------------------------------
+// Bottons
 const startButton = document.querySelector(".start");
 const pauseButton = document.querySelector(".pause");
 const stopButton = document.querySelector(".stop");
 const newButton = document.querySelector(".new");
 
-//Listeners слушатели ------------------------------------------------
+//Listeners слушатели
 startButton.addEventListener("click", () => {
   clearInterval(interval); //очищаем на случай ошибок
-  interval = setInterval(startTimer, 10);//присваем первым аргументом функцию и интервал в 10 
+  interval = setInterval(startTimer, 10); //присваем первым аргументом функцию и интервал в 10
 });
-// Pause button вешаю обработчик событий на клик так же стрелочная функция клеар интервал--------------------------------------------------
+// Pause button вешаю обработчик событий на клик так же стрелочная функция клеар интервал
 pauseButton.addEventListener("click", () => {
   clearInterval(interval);
-  disabledBtn()
+  disabledBtn();
 });
-//stopButton вешаю обработчик событий на клик мтрелочную функцию и клеар наш интервал-------------------------------------------------
+//stopButton вешаю обработчик событий на клик на стрелочную функцию и клеар наш интервал
 stopButton.addEventListener("click", () => {
   clearInterval(interval);
+
   clearFields();
-  disabledBtn()
+  disabledBtn();
 });
 
-// new button--------------------------------------------
+// new button //вешаем обработчика
 newButton.addEventListener("click", () => {
-  clearInterval(interval);
+  clearInterval(interval); //очищаем интервал чтобы избежать проблем
   counter++;
-  const results = document.querySelector(".results");
-  const block = document.createElement("div");
+  const results = document.querySelector(".results"); //получить данные кнопки result
+  const block = document.createElement("div"); //помещать в резуль элемент
   block.classList.add("results__info");
-  block.innerText = `Results:${counter}:${hour}:${minute}:${second}:${millisecond}`;
-  results.append(block);
+  block.innerText = `Result ${counter}: ${hour}:${minute}:${second}:${millisecond}`;
+  results.append(block); // и мы в наш резулт через метод аппенд помещаю блок
   clearFields();
   clearInterval(interval);
   interval = setInterval(startTimer, 10); // функция старта
 });
 
-//Variables----------------------------------------------
+//Variables
 let hour = 00,
   minute = 00,
   second = 00,
@@ -53,57 +54,65 @@ let hour = 00,
   disabled = true;
 //создаю функцию отвечающую за наш таймер
 function startTimer() {
-  millisecond++;//увеличиваем на 1
+  millisecond++; //увеличиваем на 1
 
-  //Millisecond------------------------------------------
-  if (millisecond < 9) {// если милисекунд меньше 9 millisecondElement.innerText = "0" + millisecond;
+  //Millisecond
+  if (millisecond < 9) {
+    // если милисекунд меньше 9 millisecondElement.innerText = "0" + millisecond;
     millisecondElement.innerText = "0" + millisecond;
   }
-  if (millisecond > 9) { //если больше 9 millisecondElement.innerText = millisecond; через innerText помещаем просто милисекунды
+  if (millisecond > 9) {
+    //если больше 9 millisecondElement.innerText = millisecond; через innerText помещаем просто милисекунды
     secondElement.innerText = "0" + second;
     millisecondElement.innerText = millisecond;
   }
-  if (millisecond > 99) { // если милисекунды больше 99  second++;
+  if (millisecond > 99) {
+    // если милисекунды больше 99  second++;
     second++;
     secondElement.innerText = "0" + second;
     millisecond = 0;
     millisecondElement.innerText = "0" + millisecond;
   }
 
-  //Second-------------------------------------------------
-  if (second < 9) { //если секунд меньше 9 то берем секонделемет через инертекс и помещаю 0+ секонд
+  //Second
+  if (second < 9) {
+    //если секунд меньше 9 то берем секонделемет через инертекс и помещаю 0+ секонд
     secondElement.innerText = "0" + second;
   }
-  if (secondElement > 9) { // если секунд больше 9 то secondElement.innerText = second;
+  if (secondElement > 9) {
+    // если секунд больше 9 то secondElement.innerText = second;
     secondElement.innerText = second;
   }
-  if (second > 59) { //если секунд больше 59 то увеличиваю минуты ++и буду обновлять
+  if (second > 59) {
+    //если секунд больше 59 то увеличиваю минуты ++и буду обновлять
     minute++;
     minuteElement.innerText = "0" + minute;
     second = 0;
     secondElement.innerText = "0" + second;
   }
-  // Minute------------------------------------
-  if (minute < 9) {// смотри выше всё похоже
+  // Minute
+  if (minute < 9) {
+    // смотри выше всё похоже
     minuteElement.innerText = "0" + minute;
   }
-  if (minuteElement > 9 ) {
-      minuteElement.innerText = minute
+  if (minuteElement > 9) {
+    minuteElement.innerText = minute;
   }
-  if (minute > 60 ) {
-      hour++
-      hourElement.innerText = '0' + hour
-      minute = 0
-      minuteElement.innerText = '0' + minute
+  if (minute > 60) {
+    hour++;
+    hourElement.innerText = "0" + hour;
+    minute = 0;
+    minuteElement.innerText = "0" + minute;
   }
 
-  //Hour------------------------------------------------
+  //Hour
   if (hour < 9) {
     hourElement.innerText = "0" + hour;
   }
-newButton.disabled = false
+  newButton.disabled = false;
 }
 function clearFields() {
+  //
   (hour = 00),
     (minute = 00),
     (second = 00),
@@ -113,9 +122,9 @@ function clearFields() {
   secondElement.textContent = "00";
   millisecondElement.textContent = "00";
 }
-function disabledBtn () {
-    if (disabled) {
-        newButton.disabled = true
-    }
+function disabledBtn() {
+  if (disabled) {
+    newButton.disabled = true;
+  }
 }
-disabledBtn()
+disabledBtn();
